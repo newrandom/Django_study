a6189048b9e343aae1f78aaa2269c0d7fb3b1779
Koong_i's github <mybac1234@naver.com>
2022-08-24 12:08:04 +0900

    lecture upload
    - youtube _ 생활코딩 Django 강의 업로드

---
+++ myapp/views.py
@@ -0,0 +1,179 @@
+from http.client import HTTPResponse
+from django.shortcuts import render, HttpResponse
+
+# ========= 추가 ========== #
+# import random   
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect
+
+# ======== 딕셔너리형태로 Read 하기 ======== #
+nextId = 4
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'model', 'body':'Model is ..'},
+]
+
+# ==== HTML Template 추가 ==== #
+def HTMLTemplate(articleTag, id=None) :
+    # === 전역변수 설정 === #
+    global topics
+
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li>
+                <form action="/delete/" method = "post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type = "submit" value="delete">
+                </form>
+            </li>
+            <li><a href="/update/{id}">update</a></li>
+        '''
+
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">\
+            {topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ol>
+            {ol}
+        </ol>
+        {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+            {contextUI}
+        </ul>
+    </body>
+    </html>
+    '''
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Welcome!')
+    # return HttpResponse('<h1>Random</h1>' + str(random.random()))
+    # return HttpResponse('''
+    # <html>
+    # <body>
+    #     <h1>Django</h1>
+    #     <ol>
+    #         <li>routing</li>
+    #         <li>view</li>
+    #         <li>model</li>
+    #     </ol>
+    #     <h2>Welcome</h2>
+    #     Hello, Django
+    # </body>
+    # </html>
+    # ''')
+
+    # # ===== 전역변수 설정 ==== #
+    # global topics
+    # ol = ''
+    # for topic in topics:
+    #     # ol += f'<li>{topic["title"]}</li>'          # 딕셔너리의 title 값을 가져옴
+    #     ol += f'<li><a href="/read/{topic["id"]}">\
+    #         {topic["title"]}</a></li>'
+
+    # return HttpResponse(f'''
+    # <html>
+    # <body>
+    #     <h1>Django</h1>
+    #     <ol>
+    #         {ol}
+    #     </ol>
+    #     <h2>Welcome</h2>
+    #     Hello, Django
+    # </body>
+    # </html>
+    # ''')
+    
+    # === HTML Template 적용 === #
+    article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    # === article 인자 받기 === #
+    return HttpResponse(HTMLTemplate(article))
+
+def read(request, id):
+    global topics
+    article = ''
+    for topic in topics:
+        if str(topic['id']) == id:
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    # return HttpResponse('Read' + id)  
+    return HttpResponse(HTMLTemplate(article, id))
+
+@csrf_exempt
+def create(request):
+    global nextId
+    # print('request.method', request.method)
+    if request.method == 'GET':
+        article = '''
+            <form action="/create/" method="post">
+                <p><input type="text" name = "title" placeholder = "title"></p>
+                <p><textarea name = "body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+    # return HttpResponse('Create')
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        # print(request.POST['title'])
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {'id':nextId, 'title':title, 'body':body}
+        topics.append(newTopic)
+        url = '/read/' + str(nextId)
+        nextId += 1
+
+        # return HttpResponse(request.POST['title'])
+        # return HttpResponse(HTMLTemplate('AAA'))
+        return redirect(url)
+
+@csrf_exempt
+def update(request, id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic = {
+                    'title' : topic['title'],
+                    'body' : topic['body']
+                }
+
+        article = f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name = "title" placeholder = "title" value={selectedTopic['title']}></p>
+                <p><textarea name = "body" placeholder="body">{selectedTopic['body']}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article, id))
+
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body  = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body
+        return redirect(f'/read/{id}')
+
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == 'POST':
+        id = request.POST['id']
+        newTopics = []
+
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+
+        topics = newTopics
+        return redirect('/')
